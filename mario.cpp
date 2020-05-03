3508c0406f58d5eb8c007ec8d041b58730cef76a
Tania Ditchuk <murashka7@gmail.com>
2020-05-03 15:11:11 +0300

    enter pyramid height

--- mario.cpp
+++ mario.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#define NUMBER 15
 using namespace std;
 
 int main()
 {
 	cout << "MARIO.\n";
 	int height, num;
+
+	cout << "Enter pyramid height (max =" << NUMBER << "). = \n";
+	while (true)
+	{
+		cin >> height;
+		if (!cin || height > NUMBER || height <= 0)
+		{
+			cout << "Incorrect input. Please re-enter: ";
+			cin.clear();
+			while (cin.get() != '\n');
+		}
+		else
+			break;
+	}
+
 
 	return 0;
 }
