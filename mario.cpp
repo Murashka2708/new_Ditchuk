2ef155cb6ce60c16718582e4ac82ee5193cf758c
Tania Ditchuk <murashka7@gmail.com>
2020-05-03 15:15:20 +0300

    two triangle

--- mario.cpp
+++ mario.cpp
@@ -3,13 +3,14 @@
 using namespace std;
 
 void OneTriangle(size_t height);
+void TwoTriangle(size_t height);
 
 int main()
 {
 	cout << "MARIO.\n";
 	int height, num;
 
-	cout << "Enter pyramid height (max =" << NUMBER << "). = \n";
+	cout << "Enter pyramid height (max =" << NUMBER << "). = \n";    //the user chooses numbers height
 	while (true)
 	{
 		cin >> height;
@@ -23,7 +24,7 @@ int main()
 			break;
 	}
 
-	cout << "Enter construction option (1 and 2) = ";
+	cout << "Enter construction option (1 and 2) = ";  //the user chooses 1 and 2 treangle
 	while (true)
 	{
 		cin >> num;
@@ -39,11 +40,15 @@ int main()
 	{
 		OneTriangle(height);
 	}
+	else if (num == 2)
+	{
+		TwoTriangle(height);
+	}
 
 	return 0;
 }
 
-void OneTriangle(size_t height)
+void OneTriangle(size_t height)  //1 triangle
 {
 	for (int i = 0; i < height; i++)
 	{
@@ -57,6 +62,29 @@ void OneTriangle(size_t height)
 			{
 				cout << "  ";
 			}
+		}
+		cout << endl;
+	}
+}
+
+void TwoTriangle(size_t height)  //2 triangle
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			if (i + j >= height - 1)
+				cout << "#";
+			else
+				cout << " ";
+		}
+		cout << " ";
+		for (int j = 0; j < height; j++)
+		{
+			if (i >= j)
+				cout << "#";
+			else
+				cout << " ";
 		}
 		cout << endl;
 	}
