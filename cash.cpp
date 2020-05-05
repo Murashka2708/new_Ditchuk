533ba3184b5518b84ebe185890a23f1445c5a016
Tania Ditchuk <murashka7@gmail.com>
2020-05-05 12:49:54 +0300

    check money

--- cash.cpp
+++ cash.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+bool CheckMoney(float money) {
+	if (money < 0 || !cin) {
+		cin.clear();
+		while (cin.get() != '\n');
+		return false;
+	}
+	return true;
+}
+
 int main()
 {
 
